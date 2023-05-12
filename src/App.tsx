3bb9f8bfc774cae6b0436bf191ba50c7af007f71
starcreep48 <rjhwasse@gmail.com>
2023-05-12 18:09:35 +0200

    background added

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,31 @@ import './App.css';
 import "./styles.css";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import provinces from './map_data/provinces.png'
+import background from './map_data/background.jpg'
 
 function App() {
   return (
-    // <div className="App">
-    //   <div className="img-container">
-    //     <img
-    //       src={provinces}
-    //       alt="provinces"
-    //     />
-    //   </div>
-    // </div>
-    <TransformWrapper
-      initialScale={1}
-      initialPositionX={2000}
-      initialPositionY={1000}
-      limitToBounds={false}
-      centerZoomedOut={false}
-      maxScale={8}
-      minScale={0.2}
-      centerOnInit={true}
-      zoomAnimation={{disabled: true}}
-    >
-      <TransformComponent>
-        <img src={provinces} alt="provinces" />
-      </TransformComponent>
-    </TransformWrapper>
+    <div style={{ backgroundImage:`url(${background})`,backgroundRepeat:"no-repeat",backgroundSize:"contain"
+    }}>
+      <div className="row">
+        <div className="col">
+          <TransformWrapper
+            initialScale={0.2}
+            initialPositionX={400}
+            initialPositionY={100}
+            limitToBounds={false}
+            centerZoomedOut={false}
+            maxScale={8}
+            minScale={0.2}
+            centerOnInit={false}
+          >
+            <TransformComponent>
+              <img src={provinces} alt="provinces" />
+            </TransformComponent>
+          </TransformWrapper>
+        </div>
+      </div>
+    </div>
   );
 }
 
